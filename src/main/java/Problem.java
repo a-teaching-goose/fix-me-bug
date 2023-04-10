060f1b7ef2cca9a2f57466dd2abbd79cedf85be1
Peng Du <rick.peng.du@gmail.com>
2023-04-10 12:00:24 -0700

    add solution

--- src/main/java/Problem.java
+++ src/main/java/Problem.java
@@ -1,7 +1,29 @@
-public class Problem2 {
+//  _
+// | |
+// | |__  _   _  __ _
+// | '_ \| | | |/ _` |
+// | |_) | |_| | (_| |
+// |_.__/ \__,_|\__, |
+//               __/ |
+//              |___/
+
+public class Problem {
 
     // Do not change signature (function name, parameters, return type)
     public static void bubbleSort(int[] data) {
-        // TODO: Add your solution
+        // bug was here: add input checks
+        if (data == null || data.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j]; // bug was here, data[i] to data[j]
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
     }
 }
--- src/test/java/ProblemTest.java
+++ src/test/java/ProblemTest.java
@@ -2,28 +2,58 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class Problem2Test {
+/*
+    "oh boy this really bugs me"
+ */
+
+public class ProblemTest {
 
     // Okay to change
     @Test
+    public void testBubbleSortNullInputCheck() {
+        int[] input = null;
+
+        try {
+            Problem.bubbleSort(input);
+        } catch (Exception e) {
+            assertFalse(true); // it shouldn't throw exception
+        }
+
+        assertNull(input);
+    }
+
+    @Test
+    public void testBubbleSortEmptyInputCheck() {
+        int[] input = {};
+
+        try {
+            Problem.bubbleSort(input);
+        } catch (Exception e) {
+            assertFalse(true); // it shouldn't throw exception
+        }
+
+        assertNotNull(input);
+        assertTrue(input.length==0);
+    }
+
+    @Test
     public void testBubbleSort() {
         int inputs[][] = {
+                {},
+                {3, 3, 3},
                 {3, 2, 1},
-                // TODO: add more test case inputs
+                {3, 5, -1, 9, 12, 13, 8, 7},
         };
         int expects[][] = {
+                {},
+                {3, 3, 3},
                 {1, 2, 3},
-                // TODO: add more test case expected sorted results
+                {-1, 3, 5, 7, 8, 9, 12, 13},    // bug was here
         };
 
         for (int i = 0; i < inputs.length; i++) {
-            // TODO: add a call to your bubble sort function here
-
+            Problem.bubbleSort(inputs[i]);
             for (int j = 0; j < inputs[i].length; j++) {
-
-                // this checks whether the actual result matches the expects result
-                // note the use of assertArrayEquals instead of assertEquals here
-                // less code == less chance of mistake
                 assertArrayEquals(expects[i], inputs[i]);
             }
         }
